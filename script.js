860da5a21875a6342e33ebc42690a15d4b8bc0b6
Niouk971 <florian.polter.77@gmail.com>
2025-09-08 12:25:28 +0200

    update css

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ document.querySelector('#search-button').addEventListener('click', (e) => {
 
     console.log('hellooooo');
 });
+
+// async function loadAnimals() {
+//     try {
+//         const response = await fetch('./data/animals.json');
+//         const data = await response.json();
+//         console.log(data[0].name);
+//     } catch (error) {
+//         console.error('Erreur lors du chargement du JSON :', error);
+//     }
+// }
+
+// loadAnimals();
